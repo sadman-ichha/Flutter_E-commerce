af68ae1088470b23fc89779d836eca4a045913a8
sadman_ichha <sadmanichha1@gmail.com>
2022-10-09 18:15:59 +0600

    commit

--- lib/ui/views/bottom_nav_pages/home.dart
+++ lib/ui/views/bottom_nav_pages/home.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:e_commerce/const/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -9,10 +12,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
+      appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: true,
         backgroundColor: Colors.transparent,
@@ -25,12 +29,45 @@ class _HomeState extends State<Home> {
               fontWeight: FontWeight.w600),
         ),
       ),
-     
-      body: Column(
-        children: [
-          SizedBox(height: 100.0.h),
-          Center(),
-        ],
+      body: SafeArea(
+        child: Container(
+         color:Colors.amber,
+            child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(0)),
+                          borderSide: BorderSide(color: Colors.blue)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(0)),
+                          borderSide: BorderSide(color: Colors.black)),
+                      hintText: "Search products here",
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 60.0.h,
+                  width: 60.0.h,
+                  decoration: BoxDecoration(
+                    color: AppColors.pastelRed,
+                  ),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.search,
+                        size: 30,
+                      )),
+                )
+              ],
+            ),
+          ],
+        )),
       ),
     );
   }
