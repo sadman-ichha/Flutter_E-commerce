ead3a8203bf4a445d46016221f1d56d47ee43a22
sadman_ichha <sadmanichha1@gmail.com>
2022-10-10 09:46:08 +0600

    commit

--- lib/ui/views/bottom_nav_pages/home.dart
+++ lib/ui/views/bottom_nav_pages/home.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:ui';
+
 import 'package:e_commerce/const/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -30,44 +32,53 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: SafeArea(
-        child: Container(
-         color:Colors.amber,
-            child: Column(
+        child: Column(
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    readOnly: true,
-                    decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(0)),
-                          borderSide: BorderSide(color: Colors.blue)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(0)),
-                          borderSide: BorderSide(color: Colors.black)),
-                      hintText: "Search products here",
+            SizedBox(height: 30.0.h),
+            Padding(
+              padding:  EdgeInsets.only(left: 25.0.w,right: 25.0.w),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 60.0.h,
+                      child: TextFormField(
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(0)),
+                              borderSide: BorderSide(color: Colors.blue)),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(0)),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          hintText: "Search products here",
+                          hintStyle: TextStyle(height: 1.3.h),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  height: 60.0.h,
-                  width: 60.0.h,
-                  decoration: BoxDecoration(
-                    color: AppColors.pastelRed,
-                  ),
-                  child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.search,
-                        size: 30,
-                      )),
-                )
-              ],
+                  Container(
+                    height: 60.0.h,
+                    width: 60.0.h,
+                    decoration: BoxDecoration(
+                      color: AppColors.pastelRed,
+                    ),
+                    child: Center(
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.search,
+                            size: 30,
+                          )),
+                    ),
+                  )
+                ],
+              ),
             ),
           ],
-        )),
+        ),
       ),
     );
   }
