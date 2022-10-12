f41a9c256b1118037845849ad2f4d546b2920b23
sadman_ichha <sadmanichha1@gmail.com>
2022-10-12 12:56:15 +0600

    userDataUpdate

--- lib/ui/views/bottom_nav_pages/profile.dart
+++ lib/ui/views/bottom_nav_pages/profile.dart
@@ -1,15 +1,135 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../const/app_colors.dart';
+import '../../widgets/custom_text_field.dart';
+import '../../widgets/pastelButton.dart';
 
 class Profile extends StatefulWidget {
-  Profile({Key? key}) : super(key: key);
-
   @override
   State<Profile> createState() => _HomeState();
 }
 
 class _HomeState extends State<Profile> {
+  DateTime currentDate = DateTime.now();
+  String? dob;
+  selectedDate(context) async {
+    final showDate = await showDatePicker(
+        context: context,
+        initialDate: currentDate,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2030));
+
+    if (showDate != null && showDate != currentDate) {
+      dob = "${showDate.day}-${showDate.month}-${showDate.year}";
+      _dobController.text = dob!;
+    }
+  }
+
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+  TextEditingController _dobController = TextEditingController();
+  TextEditingController _genderController = TextEditingController();
+  TextEditingController _ageController = TextEditingController();
+  final Stream<QuerySnapshot> userStream =
+      FirebaseFirestore.instance.collection("users-form-data").snapshots();
+
+  setUserData() {
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 100.0.h),
+        Text(
+          "Submit the form to continue.",
+          style: TextStyle(
+            fontSize: 25.sp,
+            fontWeight: FontWeight.w400,
+            color: AppColors.pastelRed,
+          ),
+        ),
+        SizedBox(height: 2.0.h),
+        Text(
+          "We will not share your information with anyone.",
+          style: TextStyle(
+            fontSize: 14.sp,
+            fontWeight: FontWeight.w300,
+            color: Colors.black87,
+          ),
+        ),
+        SizedBox(height: 80.0.h),
+        customTextField("Name", _nameController, TextInputType.name),
+        SizedBox(height: 10.0.h),
+        customTextField("Phone Number", _phoneController, TextInputType.phone),
+        SizedBox(height: 10.0.h),
+        TextField(
+          controller: _dobController,
+          readOnly: true,
+          decoration: InputDecoration(
+            hintText: "Date of Birth",
+            hintStyle:
+                TextStyle(fontSize: 17.0.sp, fontWeight: FontWeight.w300),
+            suffixIcon: IconButton(
+              onPressed: () => selectedDate(context),
+              icon: Image.asset(
+                'assets/icons/calendar.png',
+                width: 22.0.w,
+                height: 22.0.h,
+                color: Colors.black87,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 10.0.h),
+        // TextField(
+        //   controller: _genderController,
+        //   readOnly: true,
+        //   decoration: InputDecoration(
+        //     hintText: "Gender",
+        //     hintStyle:
+        //         TextStyle(fontSize: 17.0.sp, fontWeight: FontWeight.w300),
+        //     suffixIcon: DropdownButton(
+        //         items: genderItems.map((String value) {
+        //           return DropdownMenuItem(
+        //             value: value,
+        //             child: Text(value),
+        //             onTap: () {
+        //               setState(() {
+        //                 _genderController.text = value;
+        //               });
+        //             },
+        //           );
+        //         }).toList(),
+        //         onChanged: (context) {}),
+        //   ),
+        // ),
+
+        SizedBox(height: 10.0.h),
+        customTextField("Age", _ageController, TextInputType.number),
+        SizedBox(height: 140.0.h),
+        PastelRedButton(
+          "Update",
+          () {},
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(left: 35.0.w, right: 35.0.w),
+        child: SingleChildScrollView(
+          child: StreamBuilder(
+            stream: userStream,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return setUserData();
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/ui/views/product_details_screen.dart
+++ lib/ui/views/product_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:e_commerce/const/app_colors.dart';
+import 'package:e_commerce/ui/widgets/pastelButton.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -33,56 +34,96 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           ),
         ),
         actions: [
-          CircleAvatar(
-            backgroundColor: AppColors.pastelRed,
-            child: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.favorite_border_outlined, color: Colors.white),
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: CircleAvatar(
+              backgroundColor: AppColors.pastelRed,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border_outlined, color: Colors.white),
+              ),
             ),
           ),
         ],
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 10.0.h),
-          Text("${widget._products["product-name"]}"),
-          AspectRatio(
-            aspectRatio: 3.5,
-            child: CarouselSlider(
-              options: CarouselOptions(
-                height: 155.0.h,
-                enlargeCenterPage: true,
-                enlargeStrategy: CenterPageEnlargeStrategy.scale,
-                // autoPlay: true,
-                onPageChanged: (currentIndex, carouselPageChangedReason) {
-                  setState(() {
-                    dotsPosition = currentIndex;
-                  });
-                },
-              ),
-              items: widget._products["product-img"]
-                  .map<Widget>((item) => ClipRRect(
-                        borderRadius: BorderRadius.circular(7.0.r),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(image: NetworkImage(item)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 10.0.h),
+            AspectRatio(
+              aspectRatio: 3.5,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 155.0.h,
+                  enlargeCenterPage: true,
+                  enlargeStrategy: CenterPageEnlargeStrategy.scale,
+                  // autoPlay: true,
+                  onPageChanged: (currentIndex, carouselPageChangedReason) {
+                    setState(() {
+                      dotsPosition = currentIndex;
+                    });
+                  },
+                ),
+                items: widget._products["product-img"]
+                    .map<Widget>((item) => ClipRRect(
+                          borderRadius: BorderRadius.circular(7.0.r),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(image: NetworkImage(item)),
+                            ),
                           ),
-                        ),
-                      ))
-                  .toList(),
+                        ))
+                    .toList(),
+              ),
             ),
-          ),
-          SizedBox(height: 5.0.h),
-          DotsIndicator(
-            dotsCount: widget._products["product-img"].length,
-            position: dotsPosition.toDouble(),
-            decorator: DotsDecorator(
-              color: Color.fromARGB(255, 246, 198, 194),
-              activeColor: AppColors.pastelRed,
-              size: Size(8.0.w, 8.0.h),
+            SizedBox(height: 5.0.h),
+            DotsIndicator(
+              dotsCount: widget._products["product-img"].length,
+              position: dotsPosition.toDouble(),
+              decorator: DotsDecorator(
+                color: Color.fromARGB(255, 246, 198, 194),
+                activeColor: AppColors.pastelRed,
+                size: Size(8.0.w, 8.0.h),
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 35.0.h),
+            Padding(
+              padding: EdgeInsets.only(left: 25.0.w, right: 22.0.w),
+              child: Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${widget._products["product-name"]}",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0.sp,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    //  SizedBox(height: 8.0.h),
+                    Text(
+                      "${widget._products["product-price"]}Tk",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0.sp,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(height: 15.0.h),
+                    Text(
+                      "${widget._products["product-description"]}",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17.0.sp,
+                          fontWeight: FontWeight.w300),
+                    ),
+                    SizedBox(height: 150.0.h),
+                    Center(child: PastelRedButton("Add to cart", () {})),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
