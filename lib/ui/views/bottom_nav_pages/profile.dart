b37f4f398e67209ac5773d75382b04253ed7e33c
sadman_ichha <sadmanichha1@gmail.com>
2022-10-12 16:55:39 +0600

    profile update

--- lib/ui/views/bottom_nav_pages/profile.dart
+++ lib/ui/views/bottom_nav_pages/profile.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
 import '../../../const/app_colors.dart';
 import '../../widgets/custom_text_field.dart';
 import '../../widgets/pastelButton.dart';
@@ -32,10 +33,15 @@ class _HomeState extends State<Profile> {
   TextEditingController _dobController = TextEditingController();
   TextEditingController _genderController = TextEditingController();
   TextEditingController _ageController = TextEditingController();
-  final Stream<QuerySnapshot> userStream =
-      FirebaseFirestore.instance.collection("users-form-data").snapshots();
-
-  setUserData() {
+  // final Stream userStream = FirebaseFirestore.instance
+  //     .collection("users-form-data")
+  //     .doc(FirebaseAuth.instance.currentUser!.email)
+  //     .snapshots();
+  setUserData(userData) {
+    _nameController.text = userData["name"];
+    _phoneController.text = userData["phone number"].toString();
+    _dobController.text = userData["dob"];
+    _ageController.text = userData["age"].toString();
     return Column(
       // mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -110,23 +116,55 @@ class _HomeState extends State<Profile> {
         SizedBox(height: 140.0.h),
         PastelRedButton(
           "Update",
-          () {},
+          () => updateData(),
         ),
       ],
     );
   }
 
+  updateData() {
+    try {
+      CollectionReference collectionReference =
+          FirebaseFirestore.instance.collection("users-form-data");
+      final user = FirebaseAuth.instance.currentUser!.email;
+      collectionReference
+          .doc(user)
+          .update({
+            "name": _nameController.text,
+            'phone number': _phoneController.text,
+            'dob': _dobController.value.text,
+            "age": _ageController.text,
+          })
+          .whenComplete(() => Fluttertoast.showToast(msg: "Update Successful"))
+          .then((value) => Navigator.pop(context));
+    } catch (e) {
+      Fluttertoast.showToast(msg: "Something is wrong");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(left: 35.0.w, right: 35.0.w),
-        child: SingleChildScrollView(
-          child: StreamBuilder(
-            stream: userStream,
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              return setUserData();
-            },
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.only(left: 35.0.w, right: 35.0.w),
+          child: SingleChildScrollView(
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("users-form-data")
+                  .doc(FirebaseAuth.instance.currentUser!.email)
+                  .snapshots(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                var userData = snapshot.data;
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  return setUserData(userData);
+                }
+              },
+            ),
           ),
         ),
       ),
